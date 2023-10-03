2752ac7a27862ae4b7da8dd7fc91716fef3ae543
Mark Bridges <mark@bridgetech.io>
2023-10-03 16:01:58 +0100

    Fixing when version string has a comma in it.

--- Sources/NewVersion/Version.swift
+++ Sources/NewVersion/Version.swift
@@ -9,4 +9,21 @@ public struct Version: Codable, Identifiable {
     let releaseDate: Date
     let releaseNotes: String?
     let versionString: String
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        releaseDate = try container.decode(Date.self, forKey: .releaseDate)
+        releaseNotes = try container.decodeIfPresent(String.self, forKey: .releaseNotes)
+        let versionString = try container.decode(String.self, forKey: .versionString)
+        
+        /// For some reason these can have commas in them sometimes
+        self.versionString = versionString.replacingOccurrences(of: ",", with: ".")
+    }
+    
+    init(releaseDate: Date, releaseNotes: String?, versionString: String) {
+        self.releaseDate = releaseDate
+        self.releaseNotes = releaseNotes
+        self.versionString = versionString
+    }
 }
